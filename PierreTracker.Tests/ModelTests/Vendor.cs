48ad4893647cd91d325b5002d480727e1d84eedf
Brian Noh <noh24@ymail.com>
2022-12-16 12:00:44 -0500

    rename to match project

---
+++ PierreTracker.Tests/ModelTests/Vendor.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreTracker.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PierreTracker.Tests
+{
+  [TestClass]
+  public class VenderTests 
+  {
+
+  }
+}
---
+++ PierreTracker/Models/Vendor.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace PierreTracker.Models
+{
+  public class Game
+  {
+    
+  }
+}
