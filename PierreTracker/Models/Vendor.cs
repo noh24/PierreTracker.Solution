bad5aab00da1e8a47725567de23bb73d3da4d719
Brian Noh <noh24@ymail.com>
2022-12-16 12:20:24 -0500

    clear vendor constructor test

--- PierreTracker/Models/Vendor.cs
+++ PierreTracker/Models/Vendor.cs
@@ -4,8 +4,19 @@ using System.Collections.Generic;
 
 namespace PierreTracker.Models
 {
-  public class Game
+  public class Vendor
   {
-    
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+    }
   }
 }
+//vendor (people who bought)
+// name, description, list of orders, static list vendors, assign ids, find method paid or not
+
+//order
+//title?, description, price, date, assign ids. find method
