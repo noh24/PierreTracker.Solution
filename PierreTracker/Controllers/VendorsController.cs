44c2d30dc8e32953ffa541603a11a33e6d0a5119
Brian Noh <noh24@ymail.com>
2022-12-16 18:47:14 -0500

    add route to post new orders

--- PierreTracker/Controllers/VendorsController.cs
+++ PierreTracker/Controllers/VendorsController.cs
@@ -33,5 +33,16 @@ namespace PierreTracker.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string title, string description, string date, int price)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.GetVendor(vendorId);
+      Order newOrder = new Order(title, description, date, price);
+      selectedVendor.AddOrder(newOrder);
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", selectedVendor.Orders);
+      return View("Show", model);
+    }
   }
 }
