f8ce252d408d2691c94a9982e4a0aac2e2ab4a29
Brian Noh <noh24@ymail.com>
2022-12-16 12:50:00 -0500

    test id assignment & get all

--- PierreTracker.Tests/ModelTests/VendorTests.cs
+++ PierreTracker.Tests/ModelTests/VendorTests.cs
@@ -6,16 +6,35 @@ using System.Collections.Generic;
 namespace PierreTracker.Tests
 {
   [TestClass]
-  public class VendorTests 
+  public class VendorTests :IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
-    public void VendorConstructor_ValidProperties_VendorProperties()
+    public void VendorConstructor_ReturnsNameAndDescription_VendorProperties()
     {
       string name = "Cafe1";
       string description = "Cafe";
       Vendor newVendor = new Vendor(name, description);
       Assert.AreEqual(name, newVendor.Name);
       Assert.AreEqual(description, newVendor.Description);
+    }
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      Vendor newVendor = new Vendor("name", "description");
+      Vendor newVendor2 = new Vendor("name", "description");
+      Assert.AreEqual(1, newVendor.Id);
+      Assert.AreEqual(2, newVendor2.Id);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      Vendor newVendor = new Vendor("name", "description");
+      List<Vendor> expected = new List<Vendor> { newVendor };
+      CollectionAssert.AreEqual(expected, Vendor.GetAll());
     }
   }
 }
