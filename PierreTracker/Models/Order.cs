be4f43687b7ecd15e75cd7457c8d31e75cdc6bc3
Brian Noh <noh24@ymail.com>
2022-12-16 14:06:05 -0500

    clear get id test

--- PierreTracker/Models/Order.cs
+++ PierreTracker/Models/Order.cs
@@ -8,12 +8,19 @@ namespace PierreTracker.Models
     public string Description {get; set;}
     public string DeliveryDate{get; set;}
     public int Price {get;set;}
+    public int Id {get;set;}
+    private static int _highestOrderId = 0;
     public Order(string title, string description, string deliveryDate, int price)
     {
       Title = title;
       Description = description;
       DeliveryDate = deliveryDate;
       Price = price;
+      Id = ++_highestOrderId;
+    }
+    public static void ClearAll()
+    {
+      _highestOrderId = 0;
     }
   }
 }
