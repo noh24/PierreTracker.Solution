9b205feee47844e0c36e14bf571b5e60ad508e96
Brian Noh <noh24@ymail.com>
2022-12-16 16:26:26 -0500

    use static files config

--- PierreTracker/Program.cs
+++ PierreTracker/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WordPuzzle
+namespace PierreTracker
 {
   class Program
   {
@@ -15,7 +15,7 @@ namespace WordPuzzle
 
       // app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
-
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.MapControllerRoute(
