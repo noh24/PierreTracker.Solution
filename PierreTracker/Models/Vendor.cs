8ae8d577ffcf26700d40513e45bb1b6ae72d599c
Brian Noh <noh24@ymail.com>
2022-12-16 12:50:21 -0500

    clear id assignment & get all tests

--- PierreTracker/Models/Vendor.cs
+++ PierreTracker/Models/Vendor.cs
@@ -6,12 +6,26 @@ namespace PierreTracker.Models
 {
   public class Vendor
   {
-    public string Name {get; set;}
-    public string Description {get; set;}
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; set; }
+    private static int _highestVendorId = 0;
+    private static List<Vendor> _instances = new List<Vendor> { };
+
     public Vendor(string name, string description)
     {
       Name = name;
       Description = description;
+      Id = _highestVendorId++;
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
     }
   }
 }
