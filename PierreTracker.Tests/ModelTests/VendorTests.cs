2be6dea2d8f2ce7abef379971f2c497beaf92826
Brian Noh <noh24@ymail.com>
2022-12-16 14:29:32 -0500

    test addOrder

--- PierreTracker.Tests/ModelTests/VendorTests.cs
+++ PierreTracker.Tests/ModelTests/VendorTests.cs
@@ -41,13 +41,13 @@ namespace PierreTracker.Tests
     {
       Vendor newVendor = new Vendor("name", "description");
       Vendor newVendor2 = new Vendor("name", "description");
-      Assert.AreEqual(1 , Vendor.FindIndex(2));
+      Assert.AreEqual(1, Vendor.FindIndex(2));
     }
     [TestMethod]
     public void GetVendor_ReturnsCorrectVendor_Vendor()
     {
       Vendor newVendor = new Vendor("name", "description");
-      Vendor newVendor2 = new Vendor("name", "description");   
+      Vendor newVendor2 = new Vendor("name", "description");
       Assert.AreEqual(newVendor, Vendor.GetVendor(1));
       Assert.AreEqual(newVendor2, Vendor.GetVendor(2));
     }
@@ -55,10 +55,19 @@ namespace PierreTracker.Tests
     public void DeleteVendor_RemovesVendorFromList_VendorList()
     {
       Vendor newVendor = new Vendor("name", "description");
-      Vendor newVendor2 = new Vendor("name", "description"); 
+      Vendor newVendor2 = new Vendor("name", "description");
       List<Vendor> VendorList = new List<Vendor> { newVendor };
       Vendor.DeleteVendor(2);
       CollectionAssert.AreEqual(VendorList, Vendor.GetAll());
+    }
+    [TestMethod]
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      Vendor newVendor = new Vendor("name", "description");
+      Order newOrder = new Order("title", "description", "deliveryDate", 1);
+      newVendor.AddOrder(newOrder);
+      List<Order> orderList = new List<Order> {newOrder};
+      CollectionAssert.AreEqual(orderList, newVendor.Orders);
     }
   }
 }
